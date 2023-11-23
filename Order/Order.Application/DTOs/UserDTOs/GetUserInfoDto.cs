c6631041acd9459b719164507d38d51f5501bc6d
VlazhV <danilabondarev1234@gmail.com>
2023-11-24 01:53:58 +0300

    renamed

--- Order/Order.Application/DTOs/UserDTOs/GetUserInfoDto.cs
+++ Order/Order.Application/DTOs/UserDTOs/GetUserInfoDto.cs
@@ -2,12 +2,12 @@ using Order.Application.DTOs.OrderDTOs;
 
 namespace Order.Application.DTOs.UserDTOs;
 
-public class GetUserOrdersDto
+public class GetUserInfoDto
 {
 	public long Id { get; set; }
 	public string UserName { get; set; } = null!;
 	public string Email { get; set; } = null!;
 	public string PhoneNumber { get; set; } = null!;
 
-	public List<GetOrderDto> Orders { get; set; } = null!;
+	public List<GetOrderInfoDto> Orders { get; set; } = null!;
 }
