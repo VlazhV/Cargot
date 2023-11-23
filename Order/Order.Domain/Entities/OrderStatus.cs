97eccc9a0e6e6d8826a318f13948fdd3ebfcd04a
VlazhV <danilabondarev1234@gmail.com>
2023-11-24 00:33:09 +0300

    added consts

--- Order/Order.Domain/Entities/OrderStatus.cs
+++ Order/Order.Domain/Entities/OrderStatus.cs
@@ -2,6 +2,10 @@ namespace Order.Domain.Entities;
 
 public class OrderStatus
 {
+	public const string Processing = "processing";
+	public const string Accepted = "accepted";
+	public const string Declined = "declined";
+	
 	public ushort Id { get; set; }
 	public string Name { get; set; } = null!;
 }
--- Order/Order.Infrastructure/Data/DatabaseContext.cs
+++ Order/Order.Infrastructure/Data/DatabaseContext.cs
@@ -9,4 +9,18 @@ public class DatabaseContext: DbContext
 	public DbSet<User> Users{ get; set; }
 	public DbSet<Payload> Payloads { get; set; }
 	public DbSet<OrderStatus> OrderStatuses { get; set; }
+
+	public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<OrderStatus>()
+			.HasData(
+				new OrderStatus { Id = 1, Name = OrderStatus.Processing },
+				new OrderStatus { Id = 2, Name = OrderStatus.Accepted },
+				new OrderStatus { Id = 3, Name = OrderStatus.Declined }
+			);
+	}
 }
