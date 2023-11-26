595a4b6111ece32bd057722cdf02fe497b0e85ce
VlazhV <danilabondarev1234@gmail.com>
2023-11-26 15:29:18 +0300

    added dtos

---
+++ Order/Order.Application/DTOs/PayloadDTOs/CreatePayloadDto.cs
@@ -0,0 +1,11 @@
+namespace Order.Application.DTOs.PayloadDTOs;
+
+public class CreatePayloadDto
+{
+    public int Length { get; set; }
+	public int Width { get; set; }
+	public int Height { get; set; }
+	public int Weight { get; set; }
+	
+	public string? Description { get; set; }
+}
