90fa5c702170e39e7c0d57cf0b60a60629ee818d
VlazhV <danilabondarev1234@gmail.com>
2023-11-26 19:11:16 +0300

    configs moved: Program -> Startup

--- Autopark/Autopark.Presentation/Program.cs
+++ Autopark/Autopark.Presentation/Program.cs
@@ -1,20 +1,9 @@
 using Autopark.Business.Extensions;
-using Autopark.Business.Interfaces;
-using Autopark.Business.Services;
 using Autopark.DataAccess.Data;
-using Autopark.DataAccess.Interfaces;
-using Autopark.DataAccess.Repositories;
 using Identity.Business.Extensions;
 using Microsoft.EntityFrameworkCore;
-using FluentValidation.AspNetCore;
-using FluentValidation;
-using Autopark.Business.DTOs.AutoparkDTOs;
-using Autopark.Business.Validators;
-using Autopark.Business.DTOs.CarDTOs;
-using Autopark.Business.DTOs.TrailerDTOs;
 using System.Reflection;
-using Autopark.Business.DTOs.SheduleDtos;
-using Autopark.Business.DTOs;
+using Autopark.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,26 +20,12 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
 
-builder.Services.AddScoped<ICarService, CarService>();
-builder.Services.AddScoped<ITrailerService, TrailerService>();
-builder.Services.AddScoped<IAutoparkService, AutoparkService>();
-builder.Services.AddScoped<ICarSheduleService, CarSheduleService>();
-builder.Services.AddScoped<ITrailerSheduleService, TrailerSheduleService>();
-
-builder.Services.AddScoped<ICarRepository, CarRepository>();
-builder.Services.AddScoped<ITrailerRepository, TrailerRepository>();
-builder.Services.AddScoped<IAutoparkRepository, AutoparkRepository>();
-builder.Services.AddScoped<ICarInShipSheduleRepository, CarInShipSheduleRepository>();
-builder.Services.AddScoped<ITrailerInShipSheduleRepository, TrailerInShipSheduleRepository>();
+builder.Services.ConfigureServices();
+builder.Services.ConfigureRepositories();
 
 builder.Services.AddControllers();
 
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddScoped<IValidator<UpdateAutoparkDto>, UpdateAutoparkValidator>();
-builder.Services.AddScoped<IValidator<UpdateCarDto>, UpdateCarValidator>();
-builder.Services.AddScoped<IValidator<UpdateTrailerDto>, UpdateTrailerValidator>();
-builder.Services.AddScoped<IValidator<UpdatePlanSheduleDto>, UpdatePlanSheduleValidator>();
-builder.Services.AddScoped<IValidator<SpecDto>, SpecValidator>();
+builder.Services.ConfigureValidation();
 
 builder.Services.AddIdentityService(builder.Configuration);
 
