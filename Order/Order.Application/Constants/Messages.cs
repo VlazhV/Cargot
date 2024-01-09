4cf35c15870e0ed128b1333b241174e5e765daee
VlazhV <danilabondarev1234@gmail.com>
2024-01-09 21:56:18 +0300

    added const message

--- Order/Order.Application/Constants/Messages.cs
+++ Order/Order.Application/Constants/Messages.cs
@@ -6,6 +6,7 @@ public class Messages
 	public const string PayloadIsNotFound = "Payload is not found";
 	public const string UserIsNotFound = "User is not found";
 	public const string NoPermission = "No permission";
-	public const string EmailPhoneNameIsReserved = "!Email, phone number or user name is reserved";
+	public const string EmailPhoneNameIsReserved = "Email, phone number or user name is reserved";
+	public const string IdIsReserved = "Id is reserved";
 	public const string IncorrectOrderStatus = "Incorrect status";
 }
