29cdad2011eb3e9323f379daae8c3b11908fa52e
VlazhV <danilabondarev1234@gmail.com>
2024-01-09 21:54:36 +0300

    add topics names

---
+++ Order/Order.Application/Constants/Topics.cs
@@ -0,0 +1,8 @@
+namespace Order.Application.Constants;
+
+public static class Topics
+{
+	public const string UserCreated = "UserCreated";
+	public const string UserDeleted = "UserDeleted";
+	public const string UserUpdated = "UserUpdated";
+}
