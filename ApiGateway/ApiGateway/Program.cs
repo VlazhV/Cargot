2073bfdb2f39da9c7610463019efcc49c9cd1118
VlazhV <danilabondarev1234@gmail.com>
2024-01-17 05:24:07 +0300

    refactor ocelot config

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -9,20 +9,13 @@ builder.Configuration
 	.AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureCors(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseCors();
 app.UseRouting();
 
-app.UseSwaggerForOcelotUI(opt =>
-{
-	app.UseOcelotSwagger(builder.Configuration);
-});
 
 app.UseOcelot().Wait();
 app.Run();
